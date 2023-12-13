c9a3ea4484394ea453fccebc89a690e6b61b378b
Lucasmessias1 <143418309+Lucasmessias1@users.noreply.github.com>
2023-12-13 20:22:49 -0300

    Update atividade 5.cpp

--- atividade 5.cpp
+++ atividade 5.cpp
@@ -5,68 +5,71 @@
 
 #define TAM 250
 
-struct dados
-{
+struct dados {
     char nome[200], cargo[200];
     float salario;
 };
 
-float media(struct dados funcionario[])
-{
-    int i, j;
-    float soma, resultado;
+float media(struct dados funcionario[], int total) {
+    int contador = 0;
+    float soma = 0;
 
-    for (j = 0; j < TAM; j++)
-    {
-        if (strcmp(funcionario[i].cargo, "Desenvolvedor") == 0)
-        {
-            j++;
+    for (int i = 0; i < total; i++) {
+        if (strcmp(funcionario[i].cargo, "programador") == 0) {
+            contador++;
             soma += funcionario[i].salario;
         }
     }
-    resultado = soma / (float)j;
-    return resultado;
+
+    if (contador > 0) {
+        return soma / (float)contador;
+    } else {
+        return 0;
+    }
 }
 
-int main()
-{
+int main() {
+    setlocale(LC_ALL, "portuguese");
 
-    int opcao, i;
-    
+    int opcao = 0, i = 0;
+
     struct dados funcionario[TAM];
 
-    do
-    {
+    do {
         printf("App Empresa\n");
-        printf(" 1 - Adicionar informação\n");
+        printf(" 1 - Adicionar informaÃ§Ã£o\n");
         printf(" 2 - Exibir\n");
         printf(" 3 - Sair do programa\n");
         printf("Resposta: ");
         scanf("%d", &opcao);
 
-        switch (opcao)
-        {
+        switch (opcao) {
         case 1:
             fflush(stdin);
 
-            printf("Digite o nome do %dº funcionário: ", i + 1);
-            gets(funcionario[i].nome);
+            printf("Digite o nome do %dÂº funcionÃ¡rio: ", i + 1);
+            scanf(" %s", funcionario[i].nome);
 
             printf("Digite o nome do cargo: ");
-            gets(funcionario[i].cargo);
+            scanf(" %s", funcionario[i].cargo);
 
             printf("Digite o valor do salario: ");
             scanf("%f", &funcionario[i].salario);
 
+            i++; 
             system("cls||clear");
-
             break;
+
         case 2:
-            printf("Média salarial: R$ %.2f\n", media(funcionario));
+            printf("MÃ©dia salarial de Desenvolvedores: R$ %.2f\n", media(funcionario, i));
+            break;
+
+        default:
             break;
         }
 
     } while (opcao != 3);
 
     return 0;
+}
 }
